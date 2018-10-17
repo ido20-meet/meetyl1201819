34b0cfc6feb26dbfe310b6f92c73113c31c13f47
ido20-meet <ido20@meet.mit.edu>
2018-10-17 19:23:16 +0300

    my first commit

---
+++ lab_1.py
@@ -0,0 +1,14 @@
+
+my_name = ("IDO IS DE KING")
+for i in range (100):
+	print(my_name)
+number1 = (25)
+number2 = number1 // 2
+print(number1)
+print(number2)
+sia = [2,-2,44]
+for number in sia:
+	print(number)
+	print(number)* 2
+	print(2-2+44)
+	
---
+++ lab_2.py
@@ -0,0 +1,18 @@
+
+def yeet():
+	yeet=[1, 5, 355,65]
+	lit = [yeet[0],yeet[-1]]
+	print (lit)
+yeet();
+
+ab = [1,0,2,-4,3,6,25,87]
+ba = []
+def ab1(ab):
+	for i in range(len(ab)):
+		if ab[i]< 5:
+			ba.append(ab[i])
+	return ba
+print(ab1(ab))
+
+
+
